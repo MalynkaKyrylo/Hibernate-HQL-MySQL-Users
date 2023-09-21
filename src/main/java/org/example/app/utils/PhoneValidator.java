4ee587e533942b417d574a4a84e99c27cb5e01a7
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-09-21 21:51:20 +0300

    Delete PhoneValidator class

--- src/main/java/org/example/app/utils/PhoneValidator.java
+++
@@ -1,8 +0,0 @@
-package org.example.app.utils;
-
-// Валідація телефона
-public class PhoneValidator {
-    public static boolean isPhoneValid(String phone) {
-        return phone.isEmpty() || !phone.matches(Constants.PHONE_RGX);
-    }
-}
